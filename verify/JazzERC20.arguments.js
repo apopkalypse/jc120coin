90f1827472958dbea8540041da23cf2faec143eb
Joseph Trahe <josephxtrahe@gmail.com>
2024-10-28 14:59:02 -0400

    deploy and verify

---
+++ verify/JazzERC20.arguments.js
@@ -0,0 +1,8 @@
+const settings = require("../settings.json");
+
+module.exports = [
+    settings.JazzERC20.name,
+    settings.JazzERC20.symbol,
+    settings.JazzERC20.decimals,
+    settings.JazzERC20.initialSupply
+]
